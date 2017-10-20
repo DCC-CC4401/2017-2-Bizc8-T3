df99ba52508a841c15aa194230c80e5ae0ee6165
Javier Salamanca <javier.salamanca.candia@gmail.com>
2017-10-20 10:55:02 -0300

    Add view login

--- CholitoDjango/main/urls.py
+++ CholitoDjango/main/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('apps.adoption.urls', namespace='adoption')),
+    url(r'^', include('apps.user.urls', namespace='user')),
     url(r'^admin/', admin.site.urls),
 ]
