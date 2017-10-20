43cc2241d82ce61c78764591deb2042dbdfadd9b
Javier Salamanca <javier.salamanca.candia@gmail.com>
2017-10-20 08:59:29 -0300

    Add apps

--- CholitoDjango/main/settings.py
+++ CholitoDjango/main/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.adoption',
+    'apps.animal',
+    'apps.complaint',
+    'apps.user',
 ]
 
 MIDDLEWARE = [
