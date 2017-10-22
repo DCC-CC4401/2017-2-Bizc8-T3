314e80d095994413ae14c9716d3612a3413b49e4
TakeItIsi <30667155+TakeItIsi@users.noreply.github.com>
2017-10-22 12:22:56 -0300

    Update models.py

--- CholitoDjango/apps/adoption/models.py
+++ CholitoDjango/apps/adoption/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Adoption(models.Model):
+    id = models.IntegerField()
+    animal_id = models.ForeignKey(Animal, on_delete=models.CASCADE())
+    ong_id = models.ForeignKey(ONG, on_delete=models.CASCADE())
+    arrival = models.DateTimeField
+    departure = models.DateTimeField(null=True, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE(), null=True, blank=True)
