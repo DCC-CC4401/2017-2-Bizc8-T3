fc0b2b272754b3597bd9cdf25c4551f88c06c402
TakeItIsi <30667155+TakeItIsi@users.noreply.github.com>
2017-10-22 12:22:18 -0300

    Update models.py

--- CholitoDjango/apps/animal/models.py
+++ CholitoDjango/apps/animal/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Animal(models.Model):
+    id = models.IntegerField()
+    color = models.CharField(max_length=1000)
+    name = models.CharField(max_length=1000, null=True, blank=True)
+    type= models.ForeignKey(Type_Animal, on_delete=models.CASCADE())
+    picture = models.CharField(max_length=1000, null=True, blank=True)
+    sex = models.CharField(max_length=1000)
+    age = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.id
+
+
+
+
+class Type_Animal(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
