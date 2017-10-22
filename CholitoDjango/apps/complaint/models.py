0c0a2d8e4800151e9847402f328dc8d852b15012
TakeItIsi <30667155+TakeItIsi@users.noreply.github.com>
2017-10-22 12:23:25 -0300

    Update models.py

--- CholitoDjango/apps/complaint/models.py
+++ CholitoDjango/apps/complaint/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Complaint(models.Model):
+    id = models.IntegerField()
+    place = models.CharField(max_length=1000)
+    type = models.CharField(max_length=1000)
+    animal_id = models.ForeignKey(Animal, on_delete=models.CASCADE())
+    hurt = models.BooleanField()
+    date = models.DateTimeField
+    municipality_id = models.ForeignKey(Municipality, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1000)
