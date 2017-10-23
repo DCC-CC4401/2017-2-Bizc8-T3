6c58d46a2cd717aa69078cad0e5ad5313620f825
Sebastian Donoso <sebastian.a.donoso.b@gmail.com>
2017-10-23 14:14:08 -0300

    fix some problem with models

--- CholitoDjango/apps/adoption/models.py
+++ CholitoDjango/apps/adoption/models.py
@@ -2,13 +2,13 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-#from .apps.animal.models import Animal
-#from .apps.user.models import ONG
+from .apps.animal.models import Animal
+from .apps.user.models import ONG
 
 
 class Adoption(models.Model):
-    #animal_id = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    #ong_id = models.ForeignKey(ONG, on_delete=models.CASCADE)
+    animal_id = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    ong_id = models.ForeignKey(ONG, on_delete=models.CASCADE)
     arrival = models.DateTimeField()
     departure = models.DateTimeField(null=True, blank=True)
 
