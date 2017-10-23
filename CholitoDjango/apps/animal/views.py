9569837e4c6d41ddedf4f9128ab371261e25a128
TakeItIsi <30667155+TakeItIsi@users.noreply.github.com>
2017-10-23 15:46:07 -0300

    Update views.py

--- CholitoDjango/apps/animal/views.py
+++ CholitoDjango/apps/animal/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class AnimalsforAdoptionView(TemplateView):
+
+    template_name = 'animal/animals-for-adoption-user.html'
+    def get(self, request):
+      if request.user.is_authenticated == False:
+            return render(request, self.template_name)
+        else:
+            if request.user.typeuser.name == 'Natural':
+                return render(request, self.template_name)
+            elif request.user.typeuser.name == 'ONG':
+                return HttpResponseRedirect(reverse('animal:list'))
+      
+    
+
+
+class AnimalDetailView(TemplateView):
+
+    template_name = 'animal/animal-detail.html'
+    def get(self, request):
+      if request.user.is_authenticated == False:
+            return render(request, self.template_name)
+        else:
+            if request.user.typeuser.name == 'Natural':
+                return render(request, self.template_name)
+              
+            elif request.user.typeuser.name == 'ING':
+                return HttpResponseRedirect(reverse('animal:list'))
+              
