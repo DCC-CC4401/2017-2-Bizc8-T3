bec1e04d979caad9c8ed1dc9a58f0eaa2decc636
Javier Salamanca <javier.salamanca.candia@gmail.com>
2017-10-23 02:25:35 -0300

    Edit link login

--- CholitoDjango/apps/user/views.py
+++ CholitoDjango/apps/user/views.py
@@ -21,7 +21,7 @@ class RegisterView(CreateView):
     template_name = 'user/register.html'
     form_class = RegisterForm
     # form_class = UserCreationForm
-    success_url = reverse_lazy('user:login')
+    success_url = reverse_lazy('login')
 
 
 class OngsFavoritesView(TemplateView):
