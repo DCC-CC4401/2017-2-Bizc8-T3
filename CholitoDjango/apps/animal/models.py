6f7c750190d695efec05841cd97eee75c2a33088
Sebastian Donoso <sebastian.a.donoso.b@gmail.com>
2017-10-23 20:13:20 -0300

    use adoptionr

--- CholitoDjango/apps/animal/models.py
+++ CholitoDjango/apps/animal/models.py
@@ -1,4 +1,6 @@
 import django.db
+import datetime
+
 
 class TypeAnimal(django.db.models.Model):
     name = django.db.models.CharField(max_length=100)
@@ -10,10 +12,12 @@ class TypeAnimal(django.db.models.Model):
 class Animal(django.db.models.Model):
     name = django.db.models.CharField(max_length=100, null=True, blank=True)
     type = django.db.models.ForeignKey(TypeAnimal, on_delete=django.db.models.CASCADE)
-    sex = django.db.models.CharField(max_length=1)
+    sex = django.db.models.CharField(max_length=10)
     color = django.db.models.CharField(max_length=100)
-    picture = django.db.models.CharField(max_length=100, null=True, blank=True)
+    picture = django.db.models.ImageField(upload_to='static/img/')
     age = django.db.models.CharField(max_length=3, null=True, blank=True)
-    
+    arrival = django.db.models.DateField(default=datetime.date.today, null=True)
+    adopted = django.db.models.BooleanField(default=False)
+
     def __str__(self):
         return '%s %s' % (self.name, self.color)
--- CholitoDjango/apps/animal/views.py
+++ CholitoDjango/apps/animal/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import TemplateView
+from .models import *
 
 
-class AnimalsforAdoptionView(TemplateView):
+class AnimalsforAdoptionView(View):
     template_name = 'animal/animals-for-adoption-user.html'
 
+    def get(self, request):
+        if request.user.is_authenticated == False:
+            return redirect('/')
+        else:
+            animals = Animal.objects.all()
+            context = {'animals': animals}
+            return render(request, self.template_name, context)
 
-class AnimalDetailView(TemplateView):
+
+
+
+class AnimalDetailView(View):
     template_name = 'animal/animal-detail.html'
 
-
+    def post(self, request):
+        if request.user.is_authenticated == False:
+            return redirect('/')
+        else:
+            id =request.POST['id']
+            animal = Animal.objects.get(id=id)
+            context = {'animal':animal}
+            return render(request,self.template_name,context)
