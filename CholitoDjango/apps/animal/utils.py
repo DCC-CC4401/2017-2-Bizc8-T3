762952090a793a511bf74abc60c2cbc236c6a9f1
Javier Salamanca <javier.salamanca.candia@gmail.com>
2017-10-24 00:58:11 -0300

    Delere file

--- CholitoDjango/apps/animal/utils.py
+++
@@ -1,9 +0,0 @@
-
-
-#from CholitoDjango.apps.adoption.models import Adoption
-
-
-# aca se deberia crear el modelo adoption pasando el animal y la persona
-def adopt(animalid, personid):
-    # adoption = Adoption(animal_id=animalid,person_id=personid,)
-    pass
